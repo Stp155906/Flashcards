ad84bc13c7ff23b073f9c5c0dcb384c47ea85f61
Shantalia <71754706+Stp155906@users.noreply.github.com>
2024-03-18 08:19:08 -0400

    Initial Commit

---
+++ FlashcardLabDemo/FlashcardLabDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardLabDemoApp.swift
+//  FlashcardLabDemo
+//
+//  Created by Shantalia Z on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardLabDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
